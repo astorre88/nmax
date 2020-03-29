6eb4cb0f9bd5f04009a3b8caf0bfb485ceffbff1
Dmitry Vysotsky <bigbenperl@gmail.com>
2020-03-29 14:56:48 +0300

    Add logic

--- lib/nmax.rb
+++ lib/nmax.rb
@@ -3,10 +3,30 @@
 require 'nmax/version'
 
 module Nmax
+  NUMBER_SIZE_TRESHOLD = 1000
+
   # Max number finder
   class FindMax
-    def self.call(numbers_list, num)
-      numbers_list.sort { |a, b| b <=> a }[0..num - 1]
+    def self.call(stdin, num)
+      raise ArgumentError, 'Argument should be Integer' unless num.match?(/\d/)
+
+      num = num.to_i
+
+      numbers = SortedSet.new
+      digit_sequence = String.new(capacity: NUMBER_SIZE_TRESHOLD)
+
+      stdin.each_char do |char|
+        if char.match?(/\d/)
+          digit_sequence << char
+        elsif !digit_sequence.empty?
+          numbers << digit_sequence.to_i
+          digit_sequence.clear
+        end
+      end
+
+      numbers << digit_sequence.to_i unless digit_sequence.empty?
+
+      numbers.to_a.last(num)
     end
   end
 end
--- spec/nmax_spec.rb
+++ spec/nmax_spec.rb
@@ -2,6 +2,15 @@
 
 RSpec.describe Nmax do
   it 'returns a max number' do
-    expect(Nmax::FindMax.call([2, 1, 3], 2)).to eq([3, 2])
+    input = StringIO.new
+    input << '
+      text 123 test some 34.5
+      information 1 and 133.
+      1000
+      sdfsdf
+      1d3f5
+    '
+    input.rewind
+    expect(Nmax::FindMax.call(input, '2')).to match_array([133, 1000])
   end
 end
